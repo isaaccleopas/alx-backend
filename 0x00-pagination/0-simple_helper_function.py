a76ae7af4986c5ffd47dd2e14de0c42de7bd62dd
isaaccleopas <isaaccleopas@gmail.com>
2023-08-19 12:26:55 +0000

    task 0 correction

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,6 +6,6 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Takes 2 arguements and returns start and end indices as tuple
     """
-    start = (page -1) * page_size
-    end = start + page_size
-    return (start, end)
+    start_index = (page - 1) * page_size
+    end_index = start + page_size
+    return (start_index, end_index)
