eae812fe4534c40437b817facb2601b7848dba1f
isaaccleopas <isaaccleopas@gmail.com>
2023-08-19 11:57:00 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Takes two integer arguments page and page_size"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Takes two arguements and returns start and
+    end indices as tuple
+    """
+    start_index = (page -1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
