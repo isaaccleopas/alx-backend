ba41341eba6f28e4461c527902277d86ef2d112f
isaaccleopas <isaaccleopas@gmail.com>
2023-08-19 13:25:34 +0000

    corrected Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -46,19 +46,24 @@ class Server:
         """Retrieves info about a page from a given index and with a
         specified size.
         """
-        assert index is None or 0 <= index < len(self.indexed_dataset())
-        assert isinstance(page_size, int) and page_size >= 0
-
-        indexed_data = self.indexed_dataset()
-        if index is None:
-            index = 0
-
-        next_index = index + page_size
-        data = [indexed_data[i] for i in range(index, min(next_index, len(indexed_data)))]
-
-        return {
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        page_data = []
+        data_count = 0
+        next_index = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and data_count < page_size:
+                page_data.append(item)
+                data_count += 1
+                continue
+            if data_count == page_size:
+                next_index = i
+                break
+        page_info = {
             'index': index,
             'next_index': next_index,
-            'page_size': page_size,
-            'data': data
+            'page_size': len(page_data),
+            'data': page_data,
         }
+        return page_info
