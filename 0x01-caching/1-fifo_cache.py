4dafc442c3bae4ee1c77947b9e3c409d5598467c
isaaccleopas <isaaccleopas@gmail.com>
2023-08-23 07:56:50 +0000

    task 1 added = sign

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,7 +15,7 @@ class FIFOCache(BaseCaching):
         """ Add an item in the cache using FIFO strategy
         """
         if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = self.queue.pop(0)
                 self.cache_data.pop(first_key)
                 print("DISCARD:", first_key)
