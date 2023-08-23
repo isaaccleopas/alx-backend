4f5eafe183a400f9da06c78e144753f0e52a76bd
isaaccleopas <isaaccleopas@gmail.com>
2023-08-23 09:18:06 +0000

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A caching system."""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Inherit BaseCaching class"""
+
+    def __init__(self):
+        """Call the constructor of the base class"""
+        super().__init__()
+        self.mru_order = []
+
+    def update_mru_order(self, key):
+        """Update MRU"""
+        if key in self.mru_order:
+            self.mru_order.remove(key)
+        self.mru_order.append(key)
+
+    def remove_mru_item(self):
+        """Remove MRU"""
+        if self.mru_order:
+            mru_key = self.mru_order.pop()
+            self.cache_data.pop(mru_key, None)
+
+    def put(self, key, item):
+        """ Add an item in the cache using MRU strategy
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.mru_order.pop()
+                self.cache_data.pop(mru_key)
+                print("DISCARD:", mru_key)
+
+            self.cache_data[key] = item
+            self.update_mru_order(key)
